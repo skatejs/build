8141aa4baaa715a817d69bcca3d42a9156138fb1
Joscha Feth <jfeth@atlassian.com>
2015-10-26 09:22:13 +1100

    fix: gulp-karma has been removed

--- task/perf.js
+++ task/perf.js
@@ -4,9 +4,10 @@ var gulp = require('gulp');
 var gulpBabel = require('gulp-babel');
 var gulpConcat = require('gulp-concat');
 var gulpFilter = require('gulp-filter');
-var gulpKarma = require('gulp-karma');
+var Server = require('karma').Server;
 
 var benchFilter = gulpFilter(['**', '!**/benchmark.js'], { restore: true });
+var filterEverythingExceptWebcomponents = gulpFilter(['**/*','!**/webcomponents.js/**/*'], { restore: true });
 
 module.exports = function (opts) {
   var args = [];
@@ -22,16 +23,25 @@ module.exports = function (opts) {
   return gulp.src(['node_modules/benchmark/benchmark.js', 'test/perf.js'])
     .pipe(galv.trace())
     .pipe(benchFilter)
+    .pipe(filterEverythingExceptWebcomponents)
     .pipe(galv.cache('babel', gulpBabel()))
+    .pipe(filterEverythingExceptWebcomponents.restore)
     .pipe(galv.cache('globalize', galv.globalize()))
     .pipe(gulpConcat('perf.js'))
     .pipe(gulp.dest('.tmp'))
     .pipe(benchFilter.restore)
-    .pipe(gulpKarma({
-      autoWatch: opts.watch,
-      browserNoActivityTimeout: 1000000,
-      browsers: opts.browsers.split(','),
-      client: { args: args },
-      frameworks: ['mocha', 'sinon-chai']
-    }));
+    .on('end', function() {
+      new Server({
+        singleRun: true,
+        browserNoActivityTimeout: 1000000,
+        browsers: opts.browsers.split(','),
+        client: { args: args },
+        files: [
+          '.tmp/perf.js'
+        ],
+        frameworks: ['mocha', 'sinon-chai']
+      }, function finishTaskAndExit (exitCode) {
+        process.exit(exitCode);
+      }).start();
+    });
 };
