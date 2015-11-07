9a9d26bedd89a99e6acfe075e3b27fa20e79fdad
Trey Shugart <treshugart@gmail.com>
2015-11-07 18:17:08 +1100

    Update release task.

--- task/release.js
+++ task/release.js
@@ -2,7 +2,6 @@
 
 var cwd = process.cwd();
 var path = require('path');
-var cmd = require('commander');
 var fs = require('fs');
 var pkg = require(path.join(cwd, '/package.json'));
 var semver = require('semver');
@@ -14,29 +13,22 @@ function replace (file, pattern, replacement) {
   fs.writeFileSync(file, str);
 };
 
-cmd
-  .option('-s, --semver [version]', 'The semantic version to release in lieu of --type. This takes precedence over --type.')
-  .option('-t, --type [major, minor or patch]', 'The type of release being performed in lieu of --semver.')
-  .parse(process.argv);
-
 module.exports = function () {
   var currentVersion = pkg.version;
-  var nextVersion = cmd.semver || semver.inc(
+  var nextVersion = this.semver || semver.inc(
     currentVersion,
-    cmd.type || 'patch'
+    this.type || 'patch'
   );
 
-  console.log(currentVersion, nextVersion);
-
-  // sh.exec('npm run lint');
-  // sh.exec('npm run test');
-  // replace('src/version.js', currentVersion, nextVersion);
-  // replace('bower.json', currentVersion, nextVersion);
-  // replace('package.json', currentVersion, nextVersion);
-  // sh.exec('npm run build');
-  // sh.exec('git commit -am "' + currentVersion + ' -> ' + nextVersion + '"');
-  // sh.exec('git tag -a ' + nextVersion + ' -m ' + nextVersion);
-  // sh.exec('git push');
-  // sh.exec('git push --tags');
-  // sh.exec('npm publish');
+  sh.exec('npm run lint');
+  sh.exec('npm run test');
+  replace('src/version.js', currentVersion, nextVersion);
+  replace('bower.json', currentVersion, nextVersion);
+  replace('package.json', currentVersion, nextVersion);
+  sh.exec('npm run build');
+  sh.exec('git commit -am "' + currentVersion + ' -> ' + nextVersion + '"');
+  sh.exec('git tag -a ' + nextVersion + ' -m ' + nextVersion);
+  sh.exec('git push');
+  sh.exec('git push --tags');
+  sh.exec('npm publish');
 };
