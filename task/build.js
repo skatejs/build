e4a442f7c0879d9ee995c3b4908d6f16c8b3a206
Trey Shugart <treshugart@gmail.com>
2015-11-24 13:43:33 +1100

    Update galvatron. Ensure cache is broken and proper spots are cached.

--- task/build.js
+++ task/build.js
@@ -1,10 +1,12 @@
 var del = require('del');
 var galv = require('galvatron');
+var gat = require('gulp-auto-task');
 var gulp = require('gulp');
 var gulpBabel = require('gulp-babel');
 var gulpConcat = require('gulp-concat');
 var gulpDebug = require('gulp-debug');
 var gulpFilter = require('gulp-filter');
+var gulpIf = require('gulp-if');
 var gulpUglify = require('gulp-uglify');
 
 module.exports = gulp.series(
@@ -13,24 +15,27 @@ module.exports = gulp.series(
   },
   gulp.parallel(
     function dist () {
+      var opts = gat.opts({
+        max: false
+      });
       var filterOnlySrc = gulpFilter(function (file) {
         return file.path.indexOf('/src/') > -1;
       }, { restore: true });
       return galv.trace('src/global.js').createStream()
         .pipe(filterOnlySrc)
-        .pipe(gulpBabel())
+        .pipe(galv.cache('babel', gulpBabel()))
         .pipe(filterOnlySrc.restore)
-        .pipe(galv.globalize())
+        .pipe(galv.cache('globalize', galv.globalize()))
         .pipe(gulpConcat('index.js'))
         .pipe(gulp.dest('dist'))
-        .pipe(gulpUglify())
+        .pipe(gulpIf(!opts.max, gulpUglify()))
         .pipe(gulpConcat('index.min.js'))
         .pipe(gulp.dest('dist'))
         .pipe(gulpDebug({ title: 'dist' }));
     },
     function lib () {
       return gulp.src(['src/**/*.js', '!src/global.js'])
-        .pipe(gulpBabel({ modules: 'umd' }))
+        .pipe(galv.cache('babel-umd', gulpBabel({ modules: 'umd' })))
         .pipe(gulpDebug({ title: 'lib' }))
         .pipe(gulp.dest('lib'))
     }
