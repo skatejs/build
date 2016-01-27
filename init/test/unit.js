57d00375936cee83a42a63a845d8fa3a489e5cca
Trey Shugart <treshugart@gmail.com>
2016-01-27 16:49:53 +1100

    Add initial test for version string.

--- init/test/unit.js
+++ init/test/unit.js
@@ -1,3 +1,7 @@
-describe('<%= name %>', function () {
+import version from '../src/version';
 
+describe('<%= name %>', function () {
+  it('version', function () {
+    expect(version).to.be.a('string');
+  });
 });
