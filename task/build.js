6dbe25a5535af84436c9a046267f66a036e2723b
Trey Shugart <treshugart@gmail.com>
2016-01-29 16:29:53 +1100

    Galvatron is faster and does most of the leg work. Rollup just does the bundle now.

--- task/build.js
+++ task/build.js
@@ -1,27 +1,26 @@
 var babel = require('../lib/babel');
-var babelRollup = require('../lib/babel-rollup');
 var camelcase = require('camelcase');
 var del = require('del');
 var fs = require('fs');
 var gat = require('gulp-auto-task');
+var galv = require('galvatron');
 var gulp = require('gulp');
+var gulpUtil = require('gulp-util');
 var gulpRename = require('gulp-rename');
 var gulpSourcemaps = require('gulp-sourcemaps');
 var gulpUglify = require('gulp-uglify');
 var path = require('path');
 var report = require('../lib/report');
-var rollup = require('rollup');
-var rollupCommonjs = require('rollup-plugin-commonjs');
-var rollupNpm = require('rollup-plugin-npm');
+var rollup = require('../lib/rollup');
 
 var opts = gat.opts();
-var tmpFile = 'src/global.js';
+var tmpFile = 'lib/global.js';
 var package = require(path.join(process.cwd(), 'package.json'));
 var packageMain = package['jsnext:main'] || package.main;
 var packageName = package.name;
 var packageNameVar = opts.global || camelcase(packageName);
 var noConflictAndGlobal = `
-  import main from '../${packageMain}';
+  import main from '../${package.main}';
 
   const previousGlobal = window.${packageNameVar};
   main.noConflict = function noConflict () {
@@ -33,61 +32,43 @@ var noConflictAndGlobal = `
   export default main;
 `;
 
-function log (e) {
-  console.log(e);
-}
-
-function noop (done) {
-  done();
-}
-
 module.exports = gulp.series(
   function cleanBefore () {
     return del(['dist', 'lib', tmpFile]);
   },
-  gulp.parallel(
-    gulp.series(
-      function tmp (done) {
-        fs.writeFile(tmpFile, new Buffer(noConflictAndGlobal, 'utf-8').toString(), done);
-      },
-      function dist (done) {
-        rollup.rollup({
-          entry: tmpFile,
-          plugins: [
-            babelRollup(opts.babel),
-            rollupCommonjs(),
-            rollupNpm()
-          ]
-        }).catch(log).then(function (bundle) {
-          bundle.write({
-            dest: 'dist/index.js',
-            exports: 'default',
-            format: 'umd',
-            globals: opts.globals,
-            moduleName: packageName,
-            sourceMap: true,
-            useStrict: false
-          }).catch(log).then(function () {
-            done();
-          });
-        });
-      },
-      opts.max ? noop : function uglify () {
-        return gulp.src('dist/index.js')
-          .pipe(gulpRename({ basename: 'index.min' }))
-          .pipe(gulpSourcemaps.init())
-          .pipe(report(gulpUglify()))
-          .pipe(gulpSourcemaps.write('.'))
-          .pipe(gulp.dest('dist'));
-      },
-      function cleanTmp () {
-        return del([tmpFile]);
-      }
-    ),
-    function lib () {
-      return gulp.src(['src/**/*.js'])
-        .pipe(babel(opts.babel))
-        .pipe(gulp.dest('lib'));
-    }
-  )
+  function lib () {
+    return gulp.src(['src/**/*.js'])
+      .pipe(galv.cache('lib', babel(opts.babel)))
+      .pipe(gulp.dest('lib'));
+  },
+  function createTmp () {
+    fs.writeFileSync(tmpFile, new Buffer(noConflictAndGlobal, 'utf-8').toString());
+    return gulp.src(tmpFile)
+      .pipe(galv.cache('lib-global', babel(opts.babel)))
+      .pipe(gulp.dest('lib'));
+  },
+  function dist (done) {
+    rollup(tmpFile, {
+      dest: 'dist/index.js',
+      globals: opts.globals,
+      moduleName: packageName
+    }, done);
+  },
+  function removeTmp () {
+    return del([tmpFile]);
+  },
+  opts.max ? function (done) {
+    done();
+  } : function uglify () {
+    return gulp.src('dist/index.js')
+      .pipe(gulpRename({ basename: 'index.min' }))
+      .pipe(gulpSourcemaps.init())
+      .pipe(report(gulpUglify()))
+      .pipe(gulpSourcemaps.write('.'))
+      .pipe(gulp.dest('dist'));
+  },
+  function notify (done) {
+    gulpUtil.log('Done!');
+    done();
+  }
 );
