ff8454e903d72a929145099c77a27dd98b2d5160
Trey Shugart <treshugart@gmail.com>
2016-02-11 22:09:35 +1100

    Get perf tests working again after major changes.

--- task/perf.js
+++ task/perf.js
@@ -1,49 +1,54 @@
 var assign = require('lodash/object/assign');
+var babel = require('../lib/babel');
 var galv = require('galvatron');
 var gat = require('gulp-auto-task');
 var gulp = require('gulp');
-var gulpBabel = require('gulp-babel');
 var gulpConcat = require('gulp-concat');
+var gulpDebug = require('gulp-debug');
 var gulpFilter = require('gulp-filter');
 var Server = require('karma').Server;
 
+var opts = gat.opts();
 var benchFilter = gulpFilter(['**', '!**/benchmark.js'], { restore: true });
 var filterEverythingExceptWebcomponents = gulpFilter(['**/*','!**/webcomponents.js/**/*'], { restore: true });
 
-module.exports = function (done) {
-  var args = [];
-  var opts = assign({
-    browsers: 'Firefox'
-  }, gat.opts());
 
-  if (opts.grep) {
-    args.push('--grep');
-    args.push(opts.grep);
+module.exports = gulp.series(
+  function () {
+    return galv.trace(['node_modules/skatejs-build/node_modules/benchmark/benchmark.js', 'test/perf.js']).createStream()
+      .pipe(gulpDebug())
+      .pipe(benchFilter)
+      .pipe(filterEverythingExceptWebcomponents)
+      .pipe(galv.cache('babel', babel(opts.babel)))
+      .pipe(filterEverythingExceptWebcomponents.restore)
+      .pipe(galv.cache('globalize', galv.globalize()))
+      .pipe(benchFilter.restore)
+      .pipe(gulpConcat('perf.js'))
+      .pipe(gulp.dest('.tmp'));
+  },
+  function (done) {
+    var args = [];
+    var opts = assign({
+      browsers: 'Firefox'
+    }, gat.opts());
+
+    if (opts.grep) {
+      args.push('--grep');
+      args.push(opts.grep);
+    }
+
+    new Server({
+      singleRun: true,
+      browserNoActivityTimeout: 1000000,
+      browsers: opts.browsers.split(','),
+      client: { args: args },
+      files: [
+        '.tmp/perf.js'
+      ],
+      frameworks: ['mocha', 'sinon-chai']
+    }, function finishTaskAndExit (exitCode) {
+      done();
+      process.exit(exitCode);
+    }).start();
   }
-
-  gulp.src(['node_modules/benchmark/benchmark.js', 'test/perf.js'])
-    .pipe(galv.trace())
-    .pipe(benchFilter)
-    .pipe(filterEverythingExceptWebcomponents)
-    .pipe(galv.cache('babel', gulpBabel()))
-    .pipe(filterEverythingExceptWebcomponents.restore)
-    .pipe(galv.cache('globalize', galv.globalize()))
-    .pipe(gulpConcat('perf.js'))
-    .pipe(gulp.dest('.tmp'))
-    .pipe(benchFilter.restore)
-    .on('end', function() {
-      new Server({
-        singleRun: true,
-        browserNoActivityTimeout: 1000000,
-        browsers: opts.browsers.split(','),
-        client: { args: args },
-        files: [
-          '.tmp/perf.js'
-        ],
-        frameworks: ['mocha', 'sinon-chai']
-      }, function finishTaskAndExit (exitCode) {
-        done();
-        process.exit(exitCode);
-      }).start();
-    });
-};
+);
