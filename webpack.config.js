e948fa09df5eced599a18d36a9045b6b7a75a956
Trey Shugart <treshugart@gmail.com>
2016-07-15 21:10:50 +1000

    feat(webpack): Rename bundle to index. Multple entry points for including deps and minifying. Add so
    BREAKING CHANGE: rename bundle to index and change dist/.

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,47 @@
-var camelCase = require('camelcase');
-var path = require('path');
-var webpack = require('webpack');
-var pkg = require(path.join(process.cwd(), 'package.json'));
-var shouldMininimize = process.argv.indexOf('--min') !== -1;
-var standardConfig = {
+const camelCase = require('camelcase');
+const path = require('path');
+const webpack = require('webpack');
+const pkg = require(path.join(process.cwd(), 'package.json'));
+const shouldMininimize = process.argv.indexOf('--min') !== -1;
+const standardConfig = {
+  devtool: 'source-map',
   entry: {
-    'dist/bundle.js': './src/index.js'
+    'dist/index-with-deps.js': './src/index.js',
+    'dist/index-with-deps.min.js': './src/index.js',
   },
   output: {
     path: './',
     filename: '[name]',
     libraryTarget: 'umd',
-    library: camelCase(pkg.name),
-    sourceMapFilename: '[file].map'
+    library: pkg['build:global'] || camelCase(pkg.name),
+    sourceMapFilename: '[file].map',
   },
   module: {
     loaders: [{
       test: /\.css$/,
-      loader: 'style!css'
+      loader: 'style!css',
     }, {
       test: /\.less$/,
-      loader: 'style!css!less'
+      loader: 'style!css!less',
     }, {
       loader: 'babel-loader',
       test: /\.js$/,
       query: {
-        presets: ['babel-preset-es2015', 'babel-preset-react']
-      }
-    }]
+        presets: ['babel-preset-es2015', 'babel-preset-react'],
+      },
+    }],
   },
   plugins: [
     new webpack.optimize.UglifyJsPlugin({
       include: /\.min\.js$/,
-      minimize: true
-    })
-  ]
+      minimize: true,
+    }),
+  ],
 };
 
 if (shouldMininimize) {
   Object.assign(standardConfig.entry, {
-    'dist/bundle.min.js': './src/index.js'
+    'dist/bundle.min.js': './src/index.js',
   });
 }
 
